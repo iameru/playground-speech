67e77800c73111f275bae0f42c6f237e7ecbb925
iameru <post@i3o.eu>
2023-02-09 13:50:07 +0100

    speedrec working

--- src/components/SpeechRecPage.tsx
+++ src/components/SpeechRecPage.tsx
@@ -2,15 +2,45 @@ import { useState } from "react";
 
 const SpeechRecognition =
   window.SpeechRecognition || window.webkitSpeechRecognition;
-const recognition = new SpeechRecognition();
+
 export function SpeechRec() {
-  console.log(recognition);
-  const [speechRecActive, setSpeechRecActive] = useState(true);
+  const [speechRecActive, setSpeechRecActive] = useState(false);
+  const [recognition, setRecognition] = useState(null);
+  const [sentence, setSentence] = useState("");
+
+  function initRecognition() {
+    const rec = new SpeechRecognition();
+    rec.continuous = true;
+    rec.lang = "de-DE";
+    rec.interimResults = true;
+    rec.maxAlternatives = 1;
+    // rec.interimResults = (e) => {};
+    rec.onresult = (e) => {
+      const sentence = e.results[0][0].transcript;
+      setSentence(sentence);
+    };
+    rec.start();
+    setRecognition(rec);
+  }
+  function toggleRecognition() {
+    const recording = !speechRecActive;
+    if (recording) {
+      initRecognition();
+    } else {
+      recognition.stop();
+      setRecognition(null);
+    }
+    setSpeechRecActive(!speechRecActive);
+  }
+
   return (
     <div className="w-4/5 h-4/5 m-5 border border-gray-200 flex flex-col justify-center items-center gap-5">
       <div className="flex gap-4 items-center">
         <p className="text-lg">Speechrec</p>
-        <p className="w-20 h-20 flex justify-center items-center">
+        <p
+          className="w-20 h-20 flex justify-center items-center select-none"
+          onClick={toggleRecognition}
+        >
           {speechRecActive ? (
             <span className="bg-red-400 w-full h-full rounded-full flex justify-center items-center border-2 border-red-800">
               stop
@@ -29,11 +59,7 @@ export function SpeechRec() {
         </p>
       </div>
       <div>
-        <p className="text-sm font-bold text-center px-10">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda
-          vitae optio rem ducimus ut numquam ullam fugiat reiciendis voluptas
-          consectetur?
-        </p>
+        <p className="text-sm font-bold text-center px-10">{sentence}</p>
       </div>
     </div>
   );
