b00ff529ea874b0fb3d08dd15bb4e355b3662546
iameru <post@i3o.eu>
2023-02-09 14:26:18 +0100

    end on sentence

--- src/components/SpeechRecPage.tsx
+++ src/components/SpeechRecPage.tsx
@@ -10,18 +10,21 @@ export function SpeechRec() {
 
   function initRecognition() {
     const rec = new SpeechRecognition();
-    rec.continuous = true;
+    rec.continuous = false;
     rec.lang = "de-DE";
     rec.interimResults = true;
     rec.maxAlternatives = 1;
-    // rec.interimResults = (e) => {};
     rec.onresult = (e) => {
       const sentence = e.results[0][0].transcript;
       setSentence(sentence);
     };
+    rec.onend = (e) => {
+      setSpeechRecActive(false);
+    };
     rec.start();
     setRecognition(rec);
   }
+
   function toggleRecognition() {
     const recording = !speechRecActive;
     if (recording) {
@@ -37,7 +40,7 @@ export function SpeechRec() {
     <div className="w-4/5 h-4/5 m-5 border border-gray-200 flex flex-col justify-center items-center gap-5">
       <div className="flex gap-4 items-center">
         <p className="text-lg">Speechrec</p>
-        <p
+        <button
           className="w-20 h-20 flex justify-center items-center select-none"
           onClick={toggleRecognition}
         >
@@ -50,7 +53,7 @@ export function SpeechRec() {
               activate
             </span>
           )}
-        </p>
+        </button>
       </div>
       <div>
         <p className="text-xs text-gray-700 px-5 text-center">
