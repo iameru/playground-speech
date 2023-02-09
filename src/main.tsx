e1e9f4ac170a783441d88127ce6b6f7b086f51dd
iameru <post@i3o.eu>
2023-02-09 11:17:00 +0100

    init

---
+++ src/main.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BaseLayout } from "./BaseLayout";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <React.StrictMode>
+    <BaseLayout />
+  </React.StrictMode>
+);
